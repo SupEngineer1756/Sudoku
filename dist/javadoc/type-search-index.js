b6800b1edc0b2cffc38f622b5a1106daf9069254
SupEngineer1756 <115115206+SupEngineer1756@users.noreply.github.com>
2023-01-03 14:36:23 +0100

    Add files via upload

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"sudoku1","l":"Game"},{"p":"sudoku1","l":"GameWindow"},{"p":"sudoku1","l":"IntegerField"},{"p":"sudoku1","l":"NewWindow"},{"p":"sudoku1","l":"Scase"}];updateSearchResults();
